bfe8832fc0478df7b25acc1b99044852c1ae1926
Charuhasen <charuhasen@gmail.com>
2022-02-05 13:57:14 +0000

    Built initial UI

--- main.py
+++ main.py
@@ -1,26 +1,75 @@
-import cv2 
+from turtle import width
+import cv2
+import tkinter as tk 
+from tkinter import *
+from PIL import Image, ImageTk
 
-faceCascade = cv2.CascadeClassifier('haarcascade.xml')
-cap = cv2.VideoCapture(0)
+def createwidgets():
+    feedlabel = Label(root, bg = "steelblue", fg = "white", text = "Webcam", font = ("Comic Sans MS", 20))
+    feedlabel.grid(row = 1, column=1, padx = 10, pady = 10, columnspan = 2)
 
-while True:
-    ret,frame=cap.read()
+    global cameralabel
+    cameralabel = Label(root, bg="steelblue", borderwidth=3, relief="groove")
+    cameralabel.grid(row = 2, column=1, padx = 10, pady = 10, columnspan = 2)
 
-    cv2.imshow('Camera',frame)
+    captureBtn = Button(root, text = "Capture", command=capture, bg="Lightblue", width= 20, font=("Comic Sans MS", 15))
+    captureBtn.grid(row=4, column=1, padx=10, pady=10)
 
-    K = cv2.waitKey(1)
+    showfeed()
 
-    if K == 32:
-        fileName = 'FaceImage.jpg'
-        cv2.imwrite(fileName,frame)
-        print('Image Captured!')
-        break
+def showfeed():
+    ret, frame = root.cap.read()
+    global cameralabel
 
-img = cv2.imread('FaceImage.jpg')
-faces = faceCascade.detectMultiScale(img, 1.1, 4)
+    if ret:
+        frame = cv2.flip(frame, 1)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
 
-for (x,y,w,h) in faces:
-    FaceImg = img[y:y+h, x:x+w]
-    #To save image to drive
-    filename = 'Face' + '.jpg'
-    cv2.imwrite(filename, FaceImg)
+        videoimg = Image.fromarray(cv2image)
+
+        imgtk = ImageTk.PhotoImage(image = videoimg)
+        cameralabel.configure(image = imgtk)
+        cameralabel.imgtk = imgtk
+        cameralabel.after(10, showfeed)
+    
+    else:
+        cameralabel.configure(image= "")
+
+def capture():
+
+    fileName = 'FaceImage.jpg'
+    ret, frame = root.cap.read()
+    cv2.imwrite(fileName, frame)
+    print('Image Captured!')
+
+    img = cv2.imread('FaceImage.jpg')
+    faceCascade = cv2.CascadeClassifier('haarcascade.xml')
+    faces = faceCascade.detectMultiScale(img, 1.1, 4)
+
+    for (x,y,w,h) in faces:
+        FaceImg = img[y:y+h, x:x+w]
+        #To save image to drive
+        filename = 'Face' + '.jpg'
+        cv2.imwrite(filename, FaceImg)
+
+
+
+
+root = tk.Tk()
+
+root.cap = cv2.VideoCapture(0)
+
+width, height = 640,480
+root.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+root.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+
+root.title("FrameKart")
+root.geometry("1340x700")
+root.config(background = "sky blue")
+
+destPath = StringVar()
+imagePath = StringVar()
+
+createwidgets()
+root.mainloop()
