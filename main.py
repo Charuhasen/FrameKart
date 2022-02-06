eda785664437d2d0b643c5891b8fa4b565b6b038
Charuhasen <charuhasen@gmail.com>
2022-02-06 09:56:53 +0000

    Positioned widgets to a satisfactory level

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 
-def createwidgets():
-    feedlabel = Label(root, bg = "steelblue", fg = "white", text = "Webcam", font = ("Comic Sans MS", 20))
-    feedlabel.grid(row = 1, column=1, padx = 10, pady = 10, columnspan = 2)
 
+def createwidgets():
     global cameralabel
-    cameralabel = Label(root, bg="steelblue", borderwidth=3, relief="groove")
-    cameralabel.grid(row = 2, column=1, padx = 10, pady = 10, columnspan = 2)
+    cameralabel = Label(root, borderwidth=3, relief="groove", bg="white")
+    cameralabel.grid(row=0, column=0)
+
+    recommendationlabel = Label(root, borderwidth=3, relief="groove", text="Recommendation", bg="white", fg="black", font=("Comic Sans MS", 18))
+    recommendationlabel.place(in_= cameralabel, relx = 1.0, rely = 0.0, anchor = NE)
+
+    recommendationList = Listbox(root)
+    recommendationList.insert(0, recommendationlabel, "Option 2")
+    recommendationList.place(in_= cameralabel, relx = 1.0, rely = 0.04, anchor = NE)
 
     captureBtn = Button(root, text = "Capture", command=capture, bg="Lightblue", width= 20, font=("Comic Sans MS", 15))
-    captureBtn.grid(row=4, column=1, padx=10, pady=10)
+    captureBtn.place(in_= cameralabel, relx = 0.5, rely = 1.0, anchor = S)
+
+    #-------------------------------------------------------------
+    # recommendationText = Text(root, height = 5, width = 52)
+    # recommendationText.grid(row=0, column=1, rowspan=2)
+    # recommendationText.insert(tk.END, "Recommendation")
+
+    # listcanvas = Canvas(root, borderwidth=3, relief="groove")
+    # listcanvas.grid(row = 1, column=1)
 
     showfeed()
 
@@ -53,23 +66,20 @@ def capture():
         cv2.imwrite(filename, FaceImg)
 
 
-
-
 root = tk.Tk()
+
+width, height = root.winfo_screenwidth(), root.winfo_screenheight()
 
 root.cap = cv2.VideoCapture(0)
 
-width, height = 640,480
+# width, height = 30,30
 root.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 root.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-
 root.title("FrameKart")
-root.geometry("1340x700")
-root.config(background = "sky blue")
-
-destPath = StringVar()
-imagePath = StringVar()
+root.geometry("1440x900")
+# root.attributes('-fullscreen', True)
+root.config(background = "white")
 
 createwidgets()
 root.mainloop()
