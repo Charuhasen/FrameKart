47e4b5d86c22a54bf367108ea601cbdc23b60e70
Charuhasen <charuhasen@gmail.com>
2022-02-09 21:59:45 +0000

    Added CheckButton list for recommendations list

--- main.py
+++ main.py
@@ -3,36 +3,33 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 
-def createwidgets():
-    optionList = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"]
-    
+def createwidgets():    
     global cameralabel
-    cameralabel = Label(root, borderwidth=3, relief="groove", bg="white")
+    cameralabel = Label(root, borderwidth=0, bg="white")
     cameralabel.grid(row=0, column=0)
 
     captureBtn = Button(root, text = "Capture", command=capture, bg="Lightblue", width= 20, font=("Comic Sans MS", 15))
     captureBtn.place(in_= cameralabel, relx = 0.5, rely = 1.0, anchor = S, y= -20)
 
+    global frame
     frame = Frame(root, bg="white")
     frame.grid(row=0, column=1, sticky=NW)
 
-    recommendationlabel = Label(frame, borderwidth=3, relief="groove", text="Recommendation", bg="white", fg="black", font=("Comic Sans MS", 18))
-    recommendationlabel.pack(side=TOP)
-    
-    recommendationList = Listbox(frame, bg="white", fg="black", selectmode=SINGLE)
-    for item in optionList:
-        recommendationList.insert(0, item)
-    recommendationList.pack(side=TOP)
+    recommendationLabelText = Label(frame, borderwidth=0, relief="groove", text="Recommendations", bg="white", fg="black", font=("Arial", 25))
+    recommendationLabelText.pack(side=TOP)
 
-    #-------------------------------------------------------------
-    # recommendationText = Text(root, height = 5, width = 52)
-    # recommendationText.grid(row=0, column=1, rowspan=2)
-    # recommendationText.insert(tk.END, "Recommendation")
-
-    # listcanvas = Canvas(root, borderwidth=3, relief="groove")
-    # listcanvas.grid(row = 1, column=1)
-
+    recommendations()
     showfeed()
+
+def recommendations():
+    global frame
+    optionList = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"]
+
+    checkedItem = StringVar()
+    for item in range(len(optionList)):
+        recommendationListCheck = Checkbutton(frame, text = optionList[item], variable = checkedItem, onvalue = optionList[item],  bg="white", fg="black", font=("Arial", 20))
+        recommendationListCheck.pack(side=TOP, padx=20, pady=20)
+
 
 def rescale_frame(frame, percent=75):
     width = int(frame.shape[1] * percent/ 100)
